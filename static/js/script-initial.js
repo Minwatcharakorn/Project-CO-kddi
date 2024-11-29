2ec4f0c7647b240fb5396ab86a66814c340b61d8
JakapatJod <jakapat2332@gmail.com>
2024-11-29 16:27:23 +0700

    gg

--- static/js/script-initial.js
+++ static/js/script-initial.js
@@ -1,27 +1,46 @@
-document.getElementById('login-form').addEventListener('submit', async (e) => {
-    e.preventDefault();
+// Toggle Password Visibility
+const togglePassword = document.getElementById('togglePassword');
+const passwordField = document.getElementById('password');
 
+togglePassword.addEventListener('click', () => {
+    // Toggle the type attribute
+    const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordField.setAttribute('type', type);
+
+    // Toggle the eye icon
+    togglePassword.classList.toggle('fa-eye');
+    togglePassword.classList.toggle('fa-eye-slash');
+});
+
+// Handle Apply Button Click Event
+document.getElementById('scan-button').addEventListener('click', async () => {
     const ipStart = document.getElementById('ip-start').value.trim();
     const ipEnd = document.getElementById('ip-end').value.trim();
     const username = document.getElementById('username').value.trim();
     const password = document.getElementById('password').value.trim();
-    const subnet = document.getElementById('subnet-mask').value;
+
+    // Show the loading modal
+    const loadingModal = document.getElementById('loadingModal');
+    loadingModal.style.display = 'flex';
 
     try {
-        const response = await fetch('/api/login', {
+        const response = await fetch('/api/login_ssh', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ ipStart, ipEnd, username, password, subnet }),
+            body: JSON.stringify({ ip_start: ipStart, ip_end: ipEnd, username, password }),
         });
 
-        const data = await response.json();
         if (response.ok) {
-            alert('SSH Login successful!');
-            console.log(data.message);
+            window.location.href = "/dashboard";
         } else {
-            throw new Error(data.error);
+            const result = await response.json();
+            throw new Error(result.error || 'Login failed.');
         }
-    } catch (err) {
-        alert(`Error: ${err.message}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        alert(`Error: ${error.message}`);
+    } finally {
+        // Hide the loading modal once processing is complete
+        loadingModal.style.display = 'none';
     }
 });
